3ed775a149b2fbf1bb228ea0c2e4b1289494290e
Wasif Dawar <m.wasifdawar@gmail.com>
2024-02-13 23:56:54 +0500

    two way data-binding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -3,4 +3,22 @@ import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
 
     bind = 'Its a One way data-binding';
+    title = 'LWC Udemy';
+
+    changeHandler(event)
+    {
+        this.title = event.target.value;
+    }
+
+    address = 
+    {
+      city: "Karachi",
+      postal: "74900",
+      country: "Pakistan"
+    }
+
+    trackHandler(event)
+    {
+        this.address.city = event.target.value;
+    }
 }
