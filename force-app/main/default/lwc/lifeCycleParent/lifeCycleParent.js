09417318e5540e1e5d523b6f4e37ee35e4bfc383
Wasif Dawar <m.wasifdawar@gmail.com>
2024-02-22 23:32:16 +0500

    Life Cycle parent

---
+++ force-app/main/default/lwc/lifeCycleParent/lifeCycleParent.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class LifeCycleParent extends LightningElement {
+    constructor()
+    {
+        super()
+        console.log("Parent Constructor Called")
+    }
+}
