a350b886d6f61cbd245281520cbe5e1f301b88a4
Wasif Dawar <m.wasifdawar@gmail.com>
2024-03-05 01:44:36 +0500

    Resolve error: carousel-02

--- force-app/main/default/lwc/p2cCarosalComponent/p2cCarosalComponent.js
+++ force-app/main/default/lwc/p2cCarosalComponent/p2cCarosalComponent.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class P2cCarosalComponent extends LightningElement {}
+export default class P2cCarosalComponent extends LightningElement {
+    @api carosalDetails
+}
